4c9799014a15f59b368e055c43929147033b575c
manav <manavkhadka2004@gmail.com>
2022-09-03 19:33:16 +0530

    bubbleSort modified

--- src/bubbleSort.java
+++ src/bubbleSort.java
@@ -2,8 +2,8 @@ import java.util.Scanner;
 
 public class bubbleSort {
     public static void bubble_Sort(int arr[]){
-       for (int i=0;i<arr.length-1;i++){
-           for (int j=0;j<arr.length-i-1;i++){
+       for (int i=0;i<arr.length-1;i++){  //array.length =n
+           for (int j=0;j<arr.length-i-1;j++){
                if (arr[j]>arr[j+1]){
                    int temp=arr[j];
                    arr[j]=arr[j+1];
