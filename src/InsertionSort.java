0cc1ffa7ed35172063f088660c4f9cc6c02515de
manav <manavkhadka2004@gmail.com>
2022-09-03 21:31:53 +0530

    Day_3 java Sorting

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -2,32 +2,21 @@ import java.util.Scanner;
 
 
     public class InsertionSort{
-        public static void Insertion_Sort(int arr[], int n){
-            int i, key,j;
-            for( i=1;i<n;i++){
-                key =arr[i];
-                j=i-1;
-                while (j>-1 && arr[j]>key){
+        public static void Insertion_Sort(int arr[]){
+            for (int i=1;i<arr.length;i++){
+                int current =arr[i];//3
+                int j = i-1;//8
+                while ( j>=0 && current<arr[j]){
                     arr[j+1]=arr[j];
                     j--;
                 }
-                arr[j+1]=key;
+                arr[j+1]=current;
             }
-
         }
     public static void main (String[] args){
         //code start
-        //input
-        Scanner sc=new Scanner(System.in);
-       int n;
-       n=sc.nextInt();
-
-       int[] arr = new int[n];
-        System.out.println("Please enter the number which should be sorted");
-       for (int i=0;i<n;i++){
-           arr[i]=sc.nextInt();
-       }
-       Insertion_Sort( arr, n);
+       int[] arr = {5,2,8,3,9,6};//{2,5,3,8}//
+       Insertion_Sort( arr);
         System.out.println("THe sorted array is:");
        for (int i=0;i<arr.length;i++){
            System.out.println(arr[i]);
--- src/selectionSort.java
+++ src/selectionSort.java
@@ -1,16 +1,21 @@
 import java.util.Scanner;
 
-public class bubbleSort {
-    public static void bubble_Sort(int arr[]){
-       for (int i=0;i<arr.length-1;i++){  //array.length =n
-           for (int j=0;j<arr.length-i-1;j++){
-               if (arr[j]>arr[j+1]){
-                   int temp=arr[j];
-                   arr[j]=arr[j+1];
-                   arr[j+1]=temp;
-               }
-           }
-       }
+public class selectionSort {
+    public static void selection_Sort(int arr[],int n){
+        for (int i=0;i<n-1;i++){
+            int min_idx=i;
+            for (int j=i+1;j<n;j++){
+                if (arr[j]<arr[min_idx]){
+                    min_idx=j;
+
+                }
+                int temp=arr[min_idx];
+                arr[min_idx]=arr[i];
+                arr[i]=temp;
+
+            }
+        }
+
 
 
     }
@@ -25,7 +30,7 @@ public class bubbleSort {
         for (int i=0;i< arr.length;i++){
             System.out.println(arr[i]);
         }
-        bubble_Sort(arr);
+        selection_Sort(arr,n);
         System.out.println("the sorted array is :-");
         for (int j=0;j<arr.length;j++){
             System.out.println(arr[j]);
