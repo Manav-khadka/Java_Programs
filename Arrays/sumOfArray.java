28fcd85147ada00022f60e0089dc41e30ad3eaef
CodeBooter <manavkhadka2004@gmail.com>
2022-10-07 01:42:05 +0530

    sum of array

---
+++ Arrays/sumOfArray.java
@@ -0,0 +1,13 @@
+package Arrays;
+
+public class sumOfArray {
+    public static void main(String[] args) {
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+        System.out.println(sum);
+    }
+
+}
